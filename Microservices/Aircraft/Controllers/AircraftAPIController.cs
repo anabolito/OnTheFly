b0cb147adf8b62150899fc7a7fb89e614015410b
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-05-07 13:33:53 -0300

    crud aircraft sem validacao de  campos

--- Microservices/Aircraft/Controllers/AircraftAPIController.cs
+++ Microservices/Aircraft/Controllers/AircraftAPIController.cs
@@ -19,12 +19,35 @@ namespace AircraftAPI.Controllers
         [HttpGet]
         public ActionResult<List<Aircraft>> Get() => _aircraftAPIService.Get();
 
+        [HttpGet("{id}")]
+        public ActionResult<Aircraft> Get(string id) => _aircraftAPIService.Get(id);
+
         [HttpPost]
         public ActionResult<Aircraft> Create(Aircraft aircraft)
         {
             return _aircraftAPIService.Create(aircraft);
         }
 
+        [HttpPut("{id}")]
+        public ActionResult Update(string id, Aircraft aircraft)
+        {
+            var c = _aircraftAPIService.Get(id);
+            if (c == null) return NotFound();
+            _aircraftAPIService.Update(aircraft);
+            return Ok();
+        }
 
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            if (id == null) return NotFound();
+
+            var aircraft = _aircraftAPIService.Get(id);
+            if (aircraft == null) return NotFound();
+
+            _aircraftAPIService.Delete(id);
+            return Ok();
+        }
     }
 }
--- Microservices/Aircraft/Repositories/AircraftAPIRepository.cs
+++ Microservices/Aircraft/Repositories/AircraftAPIRepository.cs
@@ -21,11 +21,26 @@ namespace AircraftAPI.Services
             return _aircraft.Find(c => true).ToList();
         }
 
+        public Aircraft Get(string id)
+        {
+            return _aircraft.Find<Aircraft>(c => c.RAB == id).FirstOrDefault();
+        }
+
         public Aircraft Create(Aircraft aircraft)
         {
             _aircraft.InsertOne(aircraft);
             return aircraft;
         }
-      
+
+        public void Update(Aircraft aircraft)
+        {
+            _aircraft.ReplaceOne(c => c.RAB == aircraft.RAB, aircraft);
+        }
+
+        public void Delete(string id)
+        {
+            _aircraft.DeleteOne(c => c.RAB == id);
+        }
+
     }
 }
