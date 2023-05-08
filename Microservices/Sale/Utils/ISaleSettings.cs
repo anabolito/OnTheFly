c77afe5fb3b05c062a78b6d8ad73043fb65aa6f7
GBFerro <giovani.ferro@usp.br>
2023-05-08 09:50:59 -0300

    add: adicionado configuracao de mongo com ms de sale

--- Microservices/Sale/Utils/ISaleSettings.cs
+++ Microservices/Sale/Utils/ISaleSettings.cs
@@ -2,5 +2,10 @@
 {
     public interface ISaleSettings
     {
+        string SalesCollectionName { get; set; }
+        string ReservedSalesCollectionName { get; set; }
+        string DeletedSalesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DataBaseName { get; set; }
     }
 }
--- Microservices/Sale/Utils/SaleSettings.cs
+++ Microservices/Sale/Utils/SaleSettings.cs
@@ -1,6 +1,11 @@
 ﻿namespace SaleAPI.Utils
 {
-    public class SaleSettings
+    public class SaleSettings : ISaleSettings
     {
+        public string SalesCollectionName { get; set; }
+        public string ReservedSalesCollectionName { get; set; }
+        public string DeletedSalesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DataBaseName { get; set; }
     }
 }
