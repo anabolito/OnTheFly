c9e3bd78b2f9df78369e03e31583fc18def34661
GBFerro <giovani.ferro@usp.br>
2023-05-08 22:21:17 -0300

    up: atualizando retornos de requisicoes

--- OnTheFly/Controllers/FlightController.cs
+++ OnTheFly/Controllers/FlightController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Models.DTOs;
@@ -21,21 +22,52 @@ namespace OnTheFly.Controllers
 
         #region Get
         [HttpGet]
-        public ActionResult<List<Flight>> GetFlight() => _flightService.Get().Result;
+        public ActionResult<List<Flight>> GetFlight()
+        {
+            List<Flight> list = _flightService.Get().Result;
+            if (list != null)
+                return Ok(list);
+            else
+                return NoContent();
+        }
 
         [HttpGet("{departure}")]
-        public ActionResult<List<Flight>> GetFlight(string departure) => _flightService.Get(departure).Result;
+        public ActionResult<List<Flight>> GetFlight(string departure)
+        {
+            List<Flight> list = _flightService.Get(departure).Result;
+            if (list != null)
+                return Ok(list);
+            else
+                return NoContent();
+        }
+
+        [HttpGet("{iata}/{rab}/{date}")]
+        public ActionResult<List<Flight>> GetFlight(string iata, string rab, string date)
+        {
+            Flight flight = _flightService.Get(iata, rab, date).Result;
+            if (flight != null)
+                return Ok(flight);
+            else
+                return NoContent();
+        }
         #endregion
 
         #region Post
         [HttpPost]
-        public ActionResult<Flight> PostFlight(FlightDTO flight) => _flightService.Post(flight).Result;
+        public ActionResult<Flight> PostFlight(FlightDTO flightDTO)
+        {
+            Flight flight = _flightService.Post(flightDTO).Result;
+            if (flight == null)
+                return BadRequest();
+            else
+                return Ok();
+        }
         #endregion
 
         #region Put
         [HttpPut("{iata}/{rab}/{date}")]
         public ActionResult<Flight> PutFlight(string iata, string rab, string date) =>
-            _flightService.Put(iata, rab, date).Result;
+        _flightService.Put(iata, rab, date).Result;
         #endregion
 
         #region Delete
